b4328291c566c1cbf78726984c7793c609bca98a
Sebastian Henschel <sh@sinnwerkstatt.com>
2013-11-27 13:04:37 +0000

    Replaced bootstrap-toolkit by crispy_forms

--- cosinnus_todo/forms.py
+++ cosinnus_todo/forms.py
@@ -4,9 +4,6 @@ from __future__ import unicode_literals
 from django.forms.models import ModelForm
 from django.forms.widgets import RadioSelect
 
-#from bootstrap_toolkit.widgets import BootstrapDateInput
-#from cosinnus.utils.forms import BootstrapTagWidget, BootstrapUserWidget
-
 from cosinnus_todo.models import TodoEntry
 
 
@@ -17,12 +14,7 @@ class TodoEntryForm(ModelForm):
         fields = ('title', 'due_date', 'assigned_to', 'completed_by',
                   'completed_date', 'priority', 'note', 'tags', 'media_tag')
         widgets = {
-#            'assigned_to': BootstrapUserWidget(),
-#            'completed_by': BootstrapUserWidget(),
-#            'completed_date': BootstrapDateInput(),
-#            'due_date': BootstrapDateInput(),
             'priority': RadioSelect(),
-#            'tags': BootstrapTagWidget(),
         }
 
     def __init__(self, *args, **kwargs):
@@ -43,10 +35,7 @@ class TodoEntryAddForm(TodoEntryForm):
         fields = ('title', 'due_date', 'assigned_to', 'priority',
                   'note', 'tags', 'media_tag')
         widgets = {
-#            'assigned_to': BootstrapUserWidget(),
-#            'due_date': BootstrapDateInput(),
             'priority': RadioSelect(),
-#            'tags': BootstrapTagWidget(),
         }
 
     def __init__(self, *args, **kwargs):
@@ -63,9 +52,6 @@ class TodoEntryAssignForm(TodoEntryForm):
     class Meta:
         model = TodoEntry
         fields = ('assigned_to',)
-#        widgets = {
-#            'assigned_to': BootstrapUserWidget(),
-#        }
 
 
 class TodoEntryCompleteForm(TodoEntryForm):
@@ -73,10 +59,6 @@ class TodoEntryCompleteForm(TodoEntryForm):
     class Meta:
         model = TodoEntry
         fields = ('completed_by', 'completed_date',)
-#        widgets = {
-#            'completed_by': BootstrapUserWidget(),
-#            'completed_date': BootstrapDateInput(),
-#        }
 
 
 class TodoEntryNoFieldForm(TodoEntryForm):
