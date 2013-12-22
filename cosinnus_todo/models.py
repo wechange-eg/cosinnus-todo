31a642fef2e515a14f5a3c965480023ae3c7fb7a
Sebastian Henschel <sh@sinnwerkstatt.com>
2013-12-22 13:55:05 +0100

    Declared TodoEntry model python_2_unicode_compatible

--- cosinnus_todo/models.py
+++ cosinnus_todo/models.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.core.urlresolvers import reverse
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 from django.utils.timezone import now
 from django.utils.translation import ugettext_lazy as _
 
@@ -23,6 +24,7 @@ PRIORITY_CHOICES = (
 )
 
 
+@python_2_unicode_compatible
 class TodoEntry(BaseTaggableObjectModel):
 
     SORT_FIELDS_ALIASES = [
