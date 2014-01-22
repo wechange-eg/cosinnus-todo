464ce91201d5babd1dfab2f84e3fca527c678ef7
Markus Holtermann <info@markusholtermann.eu>
2014-01-22 14:08:07 +0100

    Refers core#42 -- Refactor cosinnus registries

--- cosinnus_todo/cosinnus_app.py
+++ cosinnus_todo/cosinnus_app.py
@@ -1,8 +1,17 @@
 # -*- coding: utf-8 -*-
-from django.utils.translation import ugettext_lazy as _
 
-IS_COSINNUS_APP = True
-COSINNUS_APP_NAME = 'todo'
-COSINNUS_APP_LABEL = _('Todo')
 
-DASHBOARD_WIDGETS = ['cosinnus_todo.dashboard.MyTodos']
+def register():
+    # Import here to prevent import side effects
+    from django.utils.translation import ugettext_lazy as _
+
+    from cosinnus.core.registries import (app_registry, url_registry,
+        widget_registry)
+
+    from cosinnus_todo.urls import (cosinnus_group_patterns,
+        cosinnus_root_patterns)
+
+    app_registry.register('cosinnus_todo', 'todo', _('Todos'))
+    url_registry.register('cosinnus_todo', cosinnus_root_patterns,
+        cosinnus_group_patterns)
+    widget_registry.register('todo', 'cosinnus_todo.dashboard.MyTodos')
