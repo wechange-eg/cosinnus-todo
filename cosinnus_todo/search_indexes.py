eac3dc947f1b47df65dacf3beaddbe1cf8392cf9
Markus Holtermann <info@markusholtermann.eu>
2014-05-08 21:43:50 +0200

    Refers neww/#25 -- Implement search

---
+++ cosinnus_todo/search_indexes.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from haystack import indexes
+
+from cosinnus.utils.search import BaseTaggableObjectIndex
+
+from cosinnus_todo.models import TodoEntry
+
+
+class TodoEntryIndex(BaseTaggableObjectIndex, indexes.Indexable):
+    note = indexes.CharField(model_attr='note')
+
+    def get_model(self):
+        return TodoEntry
