3912146456f9abdb28d46fa131bd9cb08bf0e0e4
Sascha Narr <saschanarr@googlemail.com>
2014-07-14 13:40:35 +0200

    Todo widget can now load task lists, no subtasks though

--- cosinnus_todo/dashboard.py
+++ cosinnus_todo/dashboard.py
@@ -30,6 +30,9 @@ class MyTodos(DashboardWidget):
     widget_name = 'mine'
 
     def get_data(self, offset=0):
+        """ Returns a tuple (data, rows_returned, has_more) of the rendered data and how many items were returned.
+            if has_more == False, the receiving widget will assume no further data can be loaded.
+         """
         if self.request.user.is_authenticated():
             count = int(self.config['amount'])
             count_subtask = int(self.config['amount_subtask'])
@@ -39,14 +42,24 @@ class MyTodos(DashboardWidget):
             grouped_tasks = defaultdict(list)
             for task in qs:
                 grouped_tasks[task.todolist.title].append(task)
-                if count != 0 and len(grouped_tasks) >= count:
-                    break
+                # collect the full set to be able to slice it!
+                #if count != 0 and len(grouped_tasks) >= count:
+                #    break
+                
+            # we actually have the full task list here and throw out all items not currently requested 
+            # (determined by count and offset). this is bad performance, sorry.
+            if count != 0 and len(grouped_tasks) >= count:
+                # (basically a dict slice)
+                keys = grouped_tasks.keys()[offset:offset+count]
+                grouped_tasks = dict([(key, grouped_tasks[key]) for key in keys])
+                
             if count_subtask != 0:
                 for subtasks in grouped_tasks.values():
                     if len(subtasks) > count_subtask:
                         more_field = {
                             'more_field': True, 
-                            'count': len(subtasks)-count_subtask
+                            'count': len(subtasks)-count_subtask,
+                            'count_total': len(subtasks)
                         }
                         subtasks[:] = subtasks[:count_subtask]
                         subtasks.append(more_field)
@@ -58,7 +71,7 @@ class MyTodos(DashboardWidget):
             'no_data': _('No todos'),
             'user': self.request.user,
         }
-        return (render_to_string('cosinnus_todo/widgets/my_todos.html', data), 0)
+        return (render_to_string('cosinnus_todo/widgets/my_todos.html', data), len(grouped_tasks), len(grouped_tasks) >= count)
 
     def get_queryset_filter(self, **kwargs):
         return super(MyTodos, self).get_queryset_filter(assigned_to=self.request.user)
