300dac1f094804c40d82fa9fdaf1f02f697071d0
Sascha Narr <saschanarr@googlemail.com>
2016-09-19 15:32:32 +0200

    HTML digest notification configurations

--- cosinnus_todo/cosinnus_notifications.py
+++ cosinnus_todo/cosinnus_notifications.py
@@ -46,6 +46,16 @@ notifications = {
         'subject_template': 'cosinnus_todo/notifications/todo_created_subject.txt',
         'signals': [todo_created],
         'default': True,
+        
+        'is_html': True,
+        'snippet_type': 'todo',
+        'event_text': _('New todo by %(sender_name)s'),
+        'subject_text': _('A new todo: "%(object_name)s" was created in %(team_name)s.'),
+        'data_attributes': {
+            'object_name': 'title', 
+            'object_url': 'get_absolute_url', 
+            'object_text': 'note',
+        },
     },  
     'todo_assigned_to_me': {
         'label': _('A todo was assigned to me'), 
@@ -53,6 +63,17 @@ notifications = {
         'subject_template': 'cosinnus_todo/notifications/assigned_to_me_subject.txt',
         'signals': [assigned_todo_to_user],
         'default': True,
+        
+        'is_html': True,
+        'snippet_type': 'todo',
+        'event_text': _('%(sender_name)s assigned a todo to you'),
+        'subject_text': _('%(sender_name)s assigned a todo to you'),
+        'sub_event_text': _('%(sender_name)s'),
+        'data_attributes': {
+            'object_name': 'title', 
+            'object_url': 'get_absolute_url', 
+            'object_text': 'note',
+        },
     },  
     'user_completed_my_todo': {
         'label': _('A todo I created was completed'), 
@@ -60,6 +81,17 @@ notifications = {
         'subject_template': 'cosinnus_todo/notifications/user_completed_my_todo_subject.txt',
         'signals': [user_completed_my_todo],
         'default': True,
+        
+        'is_html': True,
+        'snippet_type': 'todo',
+        'event_text': _('%(sender_name)s completed your todo'),
+        'subject_text': _('%(sender_name)s completed one of your todos'),
+        'sub_event_text': _('%(sender_name)s'),
+        'data_attributes': {
+            'object_name': 'title', 
+            'object_url': 'get_absolute_url', 
+            'object_text': 'note',
+        },
     },  
     'todo_comment_posted': {
         'label': _('A user commented on one of your todos'), 
@@ -67,6 +99,19 @@ notifications = {
         'subject_template': 'cosinnus_todo/notifications/todo_comment_posted_subject.txt',
         'signals': [todo_comment_posted],
         'default': True,
+        
+        'is_html': True,
+        'snippet_type': 'todo',
+        'event_text': _('%(sender_name)s commented on your todo'),
+        'subject_text': _('%(sender_name)s commented on one of your todos'),
+        'sub_event_text': _('%(sender_name)s'),
+        'data_attributes': {
+            'object_name': 'todo.title', 
+            'object_url': 'get_absolute_url', 
+            'image_url': 'todo.creator.cosinnus_profile.get_avatar_thumbnail_url', # note: receiver avatar, not creator's!
+            'sub_image_url': 'creator.cosinnus_profile.get_avatar_thumbnail_url', # the comment creators
+            'sub_object_text': 'text',
+        },
     },    
     'tagged_todo_comment_posted': {
         'label': _('A user commented on a todo you were tagged in'), 
@@ -74,6 +119,19 @@ notifications = {
         'subject_template': 'cosinnus_todo/notifications/tagged_todo_comment_posted_subject.txt',
         'signals': [tagged_todo_comment_posted],
         'default': True,
+        
+        'is_html': True,
+        'snippet_type': 'todo',
+        'event_text': _('%(sender_name)s commented on a todo you were tagged in'),
+        'subject_text': _('%(sender_name)s commented on a todo you were tagged in in %(team_name)s'),
+        'sub_event_text': _('%(sender_name)s'),
+        'data_attributes': {
+            'object_name': 'todo.title', 
+            'object_url': 'get_absolute_url', 
+            'image_url': 'todo.creator.cosinnus_profile.get_avatar_thumbnail_url', # note: receiver avatar, not creator's!
+            'sub_image_url': 'creator.cosinnus_profile.get_avatar_thumbnail_url', # the comment creators
+            'sub_object_text': 'text',
+        },
     },  
     'assigned_todo_comment_posted': {
         'label': _('A user commented on a todo you were assigned to'), 
@@ -81,5 +139,18 @@ notifications = {
         'subject_template': 'cosinnus_todo/notifications/assigned_todo_comment_posted_subject.txt',
         'signals': [assigned_todo_comment_posted],
         'default': True,
+        
+        'is_html': True,
+        'snippet_type': 'todo',
+        'event_text': _('%(sender_name)s commented on one of your assigned todos'),
+        'subject_text': _('%(sender_name)s commented on one of your assigned todos in %(team_name)s'),
+        'sub_event_text': _('%(sender_name)s'),
+        'data_attributes': {
+            'object_name': 'todo.title', 
+            'object_url': 'get_absolute_url', 
+            'image_url': 'todo.creator.cosinnus_profile.get_avatar_thumbnail_url', # note: receiver avatar, not creator's!
+            'sub_image_url': 'creator.cosinnus_profile.get_avatar_thumbnail_url', # the comment creators
+            'sub_object_text': 'text',
+        },
     },  
 }
