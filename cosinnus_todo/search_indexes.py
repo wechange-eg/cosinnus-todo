7c44525956f04ad749cc218c2c15328b88ff6e3d
Sascha Narr <saschanarr@googlemail.com>
2016-10-24 18:09:45 +0200

    Refactor search index text and add todolist name to search result

--- cosinnus_todo/search_indexes.py
+++ cosinnus_todo/search_indexes.py
@@ -9,7 +9,6 @@ from cosinnus_todo.models import TodoEntry
 
 
 class TodoEntryIndex(BaseTaggableObjectIndex, indexes.Indexable):
-    note = indexes.CharField(model_attr='note', null=True)
 
     def get_model(self):
         return TodoEntry
