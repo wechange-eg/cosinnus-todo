3c0e4ed4c128b7a7cc23008f6d007803e3bfbd1c
sascha <sn@sinnwerkstatt.com>
2019-01-30 16:55:08 +0100

    Lots of cleanup and improvements for admin views

--- cosinnus_todo/admin.py
+++ cosinnus_todo/admin.py
@@ -4,13 +4,13 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 from cosinnus_todo.models import TodoEntry, TodoList
+from cosinnus.admin import BaseTaggableAdminMixin
 
 
-class TodoEntryAdmin(admin.ModelAdmin):
-    list_display = ('title', 'created', 'creator', 'due_date',
-                    'assigned_to', 'priority', 'group', 'completed_date',
-                    'completed_by')
-    search_fields = ('name', 'note')
+class TodoEntryAdmin(BaseTaggableAdminMixin, admin.ModelAdmin):
+    list_display = BaseTaggableAdminMixin.list_display + ['priority', 'completed_date', 'assigned_to', 'todolist']
+    list_filter = BaseTaggableAdminMixin.list_filter + ['priority', ]
+    search_fields = BaseTaggableAdminMixin.search_fields + ['note',]
 
 
 admin.site.register(TodoEntry, TodoEntryAdmin)
