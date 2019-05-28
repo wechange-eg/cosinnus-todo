31c3df23a2012ed08ef9b9587fda54a1c2ed2b42
sascha <sn@sinnwerkstatt.com>
2019-05-28 17:30:54 +0200

    Update `last_action` on user actions for BaseTaggableObjects

--- cosinnus_todo/views.py
+++ cosinnus_todo/views.py
@@ -588,7 +588,9 @@ class CommentCreateView(RequireWriteMixin, FilterGroupMixin, CreateView):
         form.instance.creator = self.request.user
         form.instance.todo = self.todo
         messages.success(self.request, self.message_success)
-        return super(CommentCreateView, self).form_valid(form)
+        ret = super(CommentCreateView, self).form_valid(form)
+        self.todo.update_last_action(now(), self.request.user, save=True)
+        return ret
 
     def get_context_data(self, **kwargs):
         context = super(CommentCreateView, self).get_context_data(**kwargs)
