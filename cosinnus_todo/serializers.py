a2ea5a4cb5af274629d04d63b90410f9b85241f3
Simon Liedtke <simon.liedtke@gmail.com>
2020-09-15 10:27:11 +0200

    Changes for new API

--- cosinnus_todo/serializers.py
+++ cosinnus_todo/serializers.py
@@ -5,10 +5,9 @@ from builtins import object
 from rest_framework import serializers
 
 from cosinnus_todo.models import TodoEntry, TodoList
-from cosinnus.models.serializers.base import DateTimeL10nField
-from cosinnus.models.serializers.group import GroupSimpleSerializer
-from cosinnus.models.serializers.profile import UserSimpleSerializer
-from cosinnus.models.serializers.tagged import TagListSerializer
+from cosinnus.api.serializers.base import DateTimeL10nField
+from cosinnus.api.serializers.group import GroupSimpleSerializer
+from cosinnus.api.serializers.user import UserSerializer
 
 
 class TodoListSerializer(serializers.ModelSerializer):
@@ -23,16 +22,16 @@ class TodoEntrySerializer(serializers.ModelSerializer):
     group = GroupSimpleSerializer(many=False)
 
     created = DateTimeL10nField()
-    creator = UserSimpleSerializer(many=False)
+    creator = UserSerializer(many=False)
 
     #tags = TagListSerializer()
 
     todolist = TodoListSerializer(many=False, required=False)
 
-    assigned_to = UserSimpleSerializer(many=False, required=False)
+    assigned_to = UserSerializer(many=False, required=False)
     due_date = DateTimeL10nField()
 
-    completed_by = UserSimpleSerializer(many=False, required=False)
+    completed_by = UserSerializer(many=False, required=False)
     completed_date = DateTimeL10nField()
 
     class Meta(object):
