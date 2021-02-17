71eca2b7dd37b6db1bbbe0405cebe734647dc637
Sascha Narr <saschanarr@googlemail.com>
2021-02-17 13:28:15 +0100

    add-django-reverse-admin-for-taggable-object-admins

--- cosinnus_todo/admin.py
+++ cosinnus_todo/admin.py
@@ -4,13 +4,13 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 from cosinnus_todo.models import TodoEntry, TodoList
-from cosinnus.admin import BaseTaggableAdminMixin
+from cosinnus.admin import BaseTaggableAdmin
 
 
-class TodoEntryAdmin(BaseTaggableAdminMixin, admin.ModelAdmin):
-    list_display = BaseTaggableAdminMixin.list_display + ['priority', 'completed_date', 'assigned_to', 'todolist']
-    list_filter = BaseTaggableAdminMixin.list_filter + ['priority', ]
-    search_fields = BaseTaggableAdminMixin.search_fields + ['note',]
+class TodoEntryAdmin(BaseTaggableAdmin):
+    list_display = BaseTaggableAdmin.list_display + ['priority', 'completed_date', 'assigned_to', 'todolist']
+    list_filter = BaseTaggableAdmin.list_filter + ['priority', ]
+    search_fields = BaseTaggableAdmin.search_fields + ['note',]
 
 
 admin.site.register(TodoEntry, TodoEntryAdmin)
